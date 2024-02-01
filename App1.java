6241c6c7aafb61e0b3eb24a5b5feb0614ca51e35
Kota Tharun Reddy <tharunreddykota2001@gmail.com>
2024-02-01 14:54:57 +0530

    App1.java added with line for webhooks

--- App1.java
+++ App1.java
@@ -3,5 +3,6 @@ public class App1{
     public static void main(String[] args){
         System.out.println("Java App Pulled & Build by Jenkins");
         System.out.println("new line added for javafile");
+        System.out.println("This line added to check for webhooks");
     }
 }
